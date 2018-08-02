84b2c41d55b9de80094edf89d277f92a5ba8f39f
joe fisher <jhanajon@gmail.com>
2018-08-02 21:00:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i] >= skipValue) {
+  for (var i = 0; i < array.length; i++) {
+      if (array[i] === skipValue) {
          break;
       }
       array[i] = changeValue 
   }
    return array
 }
+
